47d1b58e51bb7611169748d93d8598d574ee6cf8
abalcer <aubalcer@gmail.com>
2016-11-24 22:41:48 +0300

    Update Album.java

--- object-base/src/com/brainacademy/music/model/Album.java
+++ object-base/src/com/brainacademy/music/model/Album.java
@@ -5,4 +5,43 @@ import java.util.Date;
 public class Album {
     private String title;
     private Date releaseAt;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseAt() {
+        return releaseAt;
+    }
+
+    public void setReleaseAt(Date releaseAt) {
+        this.releaseAt = releaseAt;
+    }
+
+    public void setReleaseAt(String releaseAt) {
+        DateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY");
+        try {
+            this.releaseAt = dateFormat.parse(releaseAt);
+        } catch (ParseException e) {
+            this.releaseAt = null;
+            e.printStackTrace();
+        }
+    }
+
+
+    public void setReleaseAt(long releaseAt) {
+        this.releaseAt = new Date(releaseAt);
+    }
+
+    public void setReleaseAt(int year, int month, int day) {
+        this.releaseAt = new Date(year, month, day);
+    }
+
+    public String toString() {
+        return "Album: " + title + ", " + releaseAt;
+    }
 }
