dc8946ffb1c5ba3cc795beb80224503f872a2190
abalcer <aubalcer@gmail.com>
2016-11-24 22:42:43 +0300

    Update Director.java

--- object-base/src/com/brainacademy/music/model/Director.java
+++ object-base/src/com/brainacademy/music/model/Director.java
@@ -1,7 +1,36 @@
 package com.brainacademy.music.model;
 
 public class Director {
-    private String name;
+    private final String name;
+
     private String phone;
     private String email;
+
+    public Director(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(final String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Director: " + name + ", " + phone + ", " + email;
+    }
 }
