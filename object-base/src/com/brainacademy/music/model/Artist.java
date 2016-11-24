0d8733dd36a72d3a2766b0c4345c2154c3bc3929
abalcer <aubalcer@gmail.com>
2016-11-24 22:08:38 +0300

    Update Artist.java

--- object-base/src/com/brainacademy/music/model/Artist.java
+++ object-base/src/com/brainacademy/music/model/Artist.java
@@ -8,4 +8,61 @@ public class Artist {
     private Manager manager;
 
     private Album[] albums;
+
+
+    public Artist() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public Album[] getAlbums() {
+        return albums;
+    }
+
+//    public void setAlbums(Album[] albums) {
+//        this.albums = albums;
+//    }
+
+    public void setAlbums(Album... albums) {
+        this.albums = albums;
+    }
+
+
+
+    public String toString() {
+        return "Artist: " + name + "\n"
+                + director + "\n"
+                + manager + "\n"
+                + Arrays.toString(albums);
+    }
 }
