8bff5adfc691ad6d256d0cb7f10ea0421405aa24
abalcer <aubalcer@gmail.com>
2016-11-29 21:51:12 +0200

    Update Main.java

--- music/src/com/brainacademy/music/Main.java
+++ music/src/com/brainacademy/music/Main.java
@@ -57,66 +57,86 @@ public class Main {
             suggestionOut.println("Enter Email: ");
             director.setEmail(scanner.nextLine());
 
-            directors[i] = director;
-        }
+public static void main(String[] args) {
 
-        suggestionOut.print("Total artists: ");
-        int totalArtists = scanner.nextInt();
+        InputStream in = System.in;
+        //InputStream in = new ByteArrayInputStream(String.join("\n", data).getBytes());
+        //PrintStream suggestionOut = new PrintStream(new ByteArrayOutputStream(1024));
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.print("Total directors: ");
+        int totalDirectors = scanner.nextInt();
         scanner.nextLine();
 
-        Artist[] artists = new Artist[totalArtists];
-        for (int i = 0; i < artists.length; i++) {
-            Artist artist = new Artist();
+        DirectorDao directorDao = new DirectorDao();
+        DirectorConsoleReader directorConsoleReader = new DirectorConsoleReader();
 
-            suggestionOut.println("Input artists № " + i);
-            suggestionOut.println("Enter Name: ");
-            artist.setName(scanner.nextLine());
-
-            suggestionOut.println("Choose Director: ");
-            for (int j = 0; j < directors.length; j++) {
-                suggestionOut.println((j + 1) + ". " + directors[j]);
-            }
-            int k = scanner.nextInt() - 1;
-            scanner.nextLine();
-
-            artist.setDirector(directors[k]);
-            artists[i] = artist;
+        for (int i = 0; i < totalDirectors; i++) {
+            Director director = directorConsoleReader.read();
+            directorDao.add(director);
         }
 
+        System.out.println(Arrays.toString(directorDao.getAll()));
 
-        System.out.println("--------- Searching ----------");
 
-        suggestionOut.print("Search: ");
-        String name = scanner.nextLine();
-
-        for (int i = 0; i < artists.length; i++) {
-            if(artists[i].getName().equals(name)) {
-                System.out.println(artists[i]);
-            }
-        }
-
-        System.out.println("--------- Sorting ----------");
-
-        String max;
-        int maxIndex;
-        for (int i = 0; i < artists.length; i++) {
-            max = artists[i].getName();
-            maxIndex = i;
-
-            for (int j = i; j < artists.length; j++) {
-                if (max.compareTo(artists[j].getName()) > 0) {
-                    max = artists[j].getName();
-                    maxIndex = j;
-                }
-            }
-
-            Artist tmp = artists[i];
-            artists[i] = artists[maxIndex];
-            artists[maxIndex] = tmp;
-        }
-
-        for (int i = 0; i < artists.length; i++) {
-            System.out.println(artists[i]);
-        }
+//        suggestionOut.print("Total artists: ");
+//        int totalArtists = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        Artist[] artists = new Artist[totalArtists];
+//        for (int i = 0; i < artists.length; i++) {
+//            Artist artist = new Artist();
+//
+//            suggestionOut.println("Input artists № " + i);
+//            suggestionOut.println("Enter Name: ");
+//            artist.setName(scanner.nextLine());
+//
+//            suggestionOut.println("Choose Director: ");
+//            for (int j = 0; j < directors.length; j++) {
+//                suggestionOut.println((j + 1) + ". " + directors[j]);
+//            }
+//            int k = scanner.nextInt() - 1;
+//            scanner.nextLine();
+//
+//            artist.setDirector(directors[k]);
+//            artists[i] = artist;
+//        }
+//
+//
+//        System.out.println("--------- Searching ----------");
+//
+//        suggestionOut.print("Search: ");
+//        String name = scanner.nextLine();
+//
+//        for (int i = 0; i < artists.length; i++) {
+//            if (artists[i].getName().equals(name)) {
+//                System.out.println(artists[i]);
+//            }
+//        }
+//
+//        System.out.println("--------- Sorting ----------");
+//
+//        String max;
+//        int maxIndex;
+//        for (int i = 0; i < artists.length; i++) {
+//            max = artists[i].getName();
+//            maxIndex = i;
+//
+//            for (int j = i; j < artists.length; j++) {
+//                if (max.compareTo(artists[j].getName()) > 0) {
+//                    max = artists[j].getName();
+//                    maxIndex = j;
+//                }
+//            }
+//
+//            Artist tmp = artists[i];
+//            artists[i] = artists[maxIndex];
+//            artists[maxIndex] = tmp;
+//        }
+//
+//        for (int i = 0; i < artists.length; i++) {
+//            System.out.println(artists[i]);
+//        }
     }
 }
