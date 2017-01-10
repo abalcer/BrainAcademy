cda5df267aa70565ec6c0d84fe111b75e884ce49
abalcer <aubalcer@gmail.com>
2017-01-10 16:16:10 +0200

    Add threads demo applications

---
+++ threads/src/com/brainacademy/threads/ThreadPoolTest.java
@@ -0,0 +1,28 @@
+package com.brainacademy.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolTest {
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            EXECUTOR_SERVICE.execute(() -> {
+                System.out.println(Thread.currentThread().getId() + ", " + Thread.currentThread().getName());
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        EXECUTOR_SERVICE.awaitTermination(10, TimeUnit.SECONDS);
+        EXECUTOR_SERVICE.shutdown();
+
+        System.out.println("Finish");
+    }
+}
---
+++ threads/src/com/brainacademy/threads/WaitNotifyTest.java
@@ -0,0 +1,60 @@
+package com.brainacademy.threads;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class WaitNotifyTest {
+    private static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread1();
+        Thread thread2 = new Thread2();
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Finish");
+    }
+
+    static class Thread1 extends Thread {
+        @Override
+        public void run() {
+            synchronized (list) {
+                System.out.println("Waiting...");
+                try {
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("List size: " + list.size());
+            }
+        }
+    }
+
+    static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                list.add((int) (Math.random() * 100));
+                try {
+                    TimeUnit.MILLISECONDS.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            synchronized (list) {
+                list.notifyAll();
+            }
+        }
+    }
+}
