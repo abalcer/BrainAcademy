a05f34678b7681e128ada9a32d95335bb1ebc48a
abalcer <aubalcer@gmail.com>
2017-01-27 12:53:16 +0200

    Add jdbc example

---
+++ jdbc-gradle/src/main/java/com/brainacademy/jdbc/Main.java
@@ -0,0 +1,29 @@
+package com.brainacademy.jdbc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class Main {
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/jdbc-test";
+    private static final String USER = "root";
+    private static final String PASS = "123";
+
+    public static void main(String[] args) {
+        Sql2o sql2o = new Sql2o(DB_URL, USER, PASS);
+        String sql =
+                "SELECT id, category, dueDate " +
+                        "FROM Task " +
+                        "WHERE category = :category";
+
+        try(Connection con = sql2o.open()) {
+            List<Task> tasks = con.createQuery(sql)
+                    .addParameter("category", "category1")
+                    .executeAndFetch(Task.class);
+
+            System.out.println(Arrays.toString(tasks.toArray()));
+        }
+    }
+}
